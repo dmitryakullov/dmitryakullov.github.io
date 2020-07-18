191aa8b7fa14aa91c82b3cbdb287468ec23c4fb5
Dimaggio224 <dima224xz@gmail.com>
2020-07-18 13:45:15 +0300

    testReact2

---
+++ my_test/react-gotapp-master/build/precache-manifest.7cb72c372a788c1424dc03b69b84fc5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1148feed0beee72fe64",
+    "url": "/static/css/main.f85bfaf6.chunk.css"
+  },
+  {
+    "revision": "b1148feed0beee72fe64",
+    "url": "/static/js/main.b1148fee.chunk.js"
+  },
+  {
+    "revision": "b5208fc6ba3c6b8b7e5b",
+    "url": "/static/css/1.3796d8e1.chunk.css"
+  },
+  {
+    "revision": "b5208fc6ba3c6b8b7e5b",
+    "url": "/static/js/1.b5208fc6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "80bc88a852b642a15d3415f291446ba3",
+    "url": "/index.html"
+  }
+];
