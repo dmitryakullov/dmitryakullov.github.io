b929320a858289d8b0bfffc56af80f91ab875990
Dimaggio224 <dima224xz@gmail.com>
2020-07-18 13:48:37 +0300

    del

--- my_test/react-gotapp-master/src/components/errorMessage/errorMessage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ErrorMessage = () => {
-    return (
-        <span>Something go wrong</span>
-    )
-}
-
-export default ErrorMessage;
--- my_test/react-gotapp-master/src/components/spinner/spinner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './spinner.css'
-
-const Spinner = () => 
-<div className="loadingio-spinner-spin-w220yqsosks"><div className="ldio-sqpe1bsg6z">
-<div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
-</div></div>;
-
-export default Spinner;
