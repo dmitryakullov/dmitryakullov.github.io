572a3a43605bc64671ef845330cd4b2d06771d3c
Dimaggio224 <dima224xz@gmail.com>
2020-07-19 18:20:13 +0300

    da

---
+++ my_test/server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen('dimaggio224xz.github.io/my_test', function () {
+    console.log('Example app listening on port 3000!');
+});
